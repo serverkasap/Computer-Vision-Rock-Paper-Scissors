d9890b3869740b8011eeb4caaf945b79523df98f
serverkasap <server.kasap@outlook.com>
2022-05-09 11:06:55 +0100

    Add camera_rps.py

---
+++ camera_rps.py
@@ -0,0 +1,51 @@
+import cv2
+from keras.models import load_model
+import numpy as np
+
+
+def get_prediction(model_name):
+    model = load_model(model_name, compile=False)
+
+    cap = cv2.VideoCapture(0)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    while True:
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(
+            frame, (224, 224), interpolation=cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) /
+                            127.0) - 1  # Normalize the image
+        data[0] = normalized_image
+        prediction = model.predict(data)
+
+        cv2.imshow('frame', frame)
+
+        # Press q to close the window
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # After the loop release the cap object
+    cap.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
+    return [prediction[0][0], prediction[0][1], prediction[0][2], prediction[0][3]]
+
+
+if __name__ == '__main__':
+    pred = get_prediction("keras_model.h5")
+
+    max_pred = pred.index(max(pred))
+
+    if max_pred == 0:
+        print("Rock => ", end='')
+    elif max_pred == 1:
+        print("Paper => ", end='')
+    elif max_pred == 2:
+        print("Scissor => ", end='')
+    else:
+        print("Nothing => ", end='')
+
+    print("Rock:", round(pred[0], 2), "Paper:", round(pred[1], 2), "Scissor:",
+          round(pred[2], 2), "Nothing:", round(pred[3], 2))
